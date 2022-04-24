33f5a466f3575fba0d088a24d03dd6dff286a9e3
chenzhangyue <chenzhangyue@weidian.com>
2022-04-24 21:41:18 +0800

    :zap: git init

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -1,4 +1,5 @@
 <template>
+  <!--  是否隐藏-->
   <div v-if="!item.hidden">
     <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
       <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path, onlyOneChild.query)">
@@ -12,14 +13,7 @@
       <template slot="title">
         <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title" />
       </template>
-      <sidebar-item
-          v-for="child in item.children"
-          :key="child.path"
-          :is-nest="true"
-          :item="child"
-          :base-path="resolvePath(child.path)"
-          class="nest-menu"
-      />
+      <sidebar-item v-for="child in item.children" :key="child.path" :is-nest="true" :item="child" :base-path="resolvePath(child.path)" class="nest-menu" />
     </el-submenu>
   </div>
 </template>
@@ -35,8 +29,12 @@ export default {
   name: 'SidebarItem',
   components: { Item, AppLink },
   mixins: [FixiOSBug],
+  created () {
+    console.log ('以下是子菜单的数据：', this.item.path)
+  },
   props: {
     // route object
+    // 路由组件的路由对象
     item: {
       type: Object,
       required: true
@@ -55,6 +53,7 @@ export default {
     return {}
   },
   methods: {
+    // 是否展示子菜单
     hasOneShowingChild (children = [], parent) {
       if (!children) {
         children = []
@@ -64,17 +63,20 @@ export default {
           return false
         } else {
           // Temp set(will be used if only has one showing child)
+          // 临时设置设置(如果只有一个显示子对象则使用)
           this.onlyOneChild = item
           return true
         }
       })
 
       // When there is only one child router, the child router is displayed by default
+      // 当只有一个子路由器时，缺省显示该子路由器
       if (showingChildren.length === 1) {
         return true
       }
 
       // Show parent if there are no child router to display
+      // 如果没有子路由器显示，则显示父路由器
       if (showingChildren.length === 0) {
         this.onlyOneChild = { ...parent, path: '', noShowingChildren: true }
         return true
@@ -83,9 +85,11 @@ export default {
       return false
     },
     resolvePath (routePath, routeQuery) {
+      // 是外部链接
       if (isExternal (routePath)) {
         return routePath
       }
+      // 是父组件路径
       if (isExternal (this.basePath)) {
         return this.basePath
       }
--- src/views/system/notice/index.vue
+++ src/views/system/notice/index.vue
@@ -25,7 +25,7 @@
       <el-col :span="1.5">
         <el-button type="success" plain icon="el-icon-edit" size="mini" :disabled="single" @click="handleUpdate" v-hasPermi="['system:notice:edit']">修改</el-button>
       </el-col>
-      <el-col :span="1.5">
+      <el-col :span="1.5"> 
         <el-button type="danger" plain icon="el-icon-delete" size="mini" :disabled="multiple" @click="handleDelete" v-hasPermi="['system:notice:remove']">删除</el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
